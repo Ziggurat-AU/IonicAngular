ab75fa47748ddabd38418cfc02c94ec15f136a90
Mohammad Farshidmehr <s3308107@student.rmit.edu.au>
2021-04-19 10:36:31 +1000

    Add WalletConnect to Angular Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import WalletConnect from '@walletconnect/client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/wallet/wallet.page.ts
+++ src/app/wallet/wallet.page.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import WalletConnect from '@walletconnect/client';
+import WalletConnectQRCodeModal from '@walletconnect/qrcode-modal';
 
 @Component({
   selector: 'app-wallet',
   templateUrl: 'wallet.page.html',
   styleUrls: ['wallet.page.scss'],
 })
-export class walletPage {
+export class walletPage implements OnInit {
   constructor() {}
+
+  ngOnInit() {
+    this.initialConnection();
+  }
+
+  initialConnection() {}
+
+  connectToWalletConnect() {
+    const connector = new WalletConnect({
+      bridge: 'https://bridge.walletconnect.org',
+    });
+    if (!connector.connected) {
+      alert(connector.connected);
+      // create new session
+      // connector.createSession().then(() => {
+      //   // get uri for QR Code modal
+      //   const uri = connector.uri;
+      //   // display QR Code modal
+      //   WalletConnectQRCodeModal.open(uri, () => {
+      //     console.log('QR Code Modal closed');
+      //   });
+      // });
+    }
+  }
 }
